f5d8022b143bad67cd6cd2a359ee5c536ecb92a3
caojiayuan <cjy632258@hotmail.com>
2019-03-10 10:31:59 +0800

    interceptors

--- client.go
+++ client.go
@@ -5,17 +5,33 @@ import (
 	"time"
 )
 
+type ConfigInterceptor func(client *http.Client)
+type RequestInterceptor func(req *http.Request)
 
 
 type Client struct {
-	http *http.Client
+	http              *http.Client
+	configInterceptor ConfigInterceptor
+	requestInterceptor RequestInterceptor
+}
+
+func (c *Client) OnConfig(on ConfigInterceptor) *Client {
+	c.configInterceptor = on
+	return c
+}
+
+func (c *Client) OnRequest(on RequestInterceptor) *Client {
+	c.requestInterceptor = on
+	return c
 }
 
 func (c *Client) Do(req *http.Request) (*http.Response, error)  {
+	c.bootRequest(req)
 	return c.http.Do(req)
 }
 
 func (c *Client) AsyncDo(req *http.Request) *resultHolder {
+	c.bootRequest(req)
 	holder := &resultHolder{
 		result: make(chan *HttpResponse),
 		state:  resultIdle,
@@ -31,10 +47,21 @@ func (c *Client) AsyncDo(req *http.Request) *resultHolder {
 	return holder
 }
 
-func NewClient() *Client {
-	return &Client{
-		http: &http.Client{
+func (c *Client) bootRequest(req *http.Request) {
+	if c.http == nil {
+		c.http = &http.Client{
 			Timeout: 5 * time.Second,
-		},
+		}
 	}
+	if c.configInterceptor != nil {
+		c.configInterceptor(c.http)
+	}
+
+	if c.requestInterceptor != nil {
+		c.requestInterceptor(req)
+	}
+}
+
+func NewClient() *Client {
+	return &Client{}
 }
